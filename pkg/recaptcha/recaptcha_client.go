ff858b9ab266c57da878741635db229183191090
sheva0914 <sheva.7.krslqkr@gmail.com>
2022-07-18 13:23:13 +0100

    feat: add recaptcha client package

---
+++ pkg/recaptcha/recaptcha_client.go
@@ -0,0 +1,48 @@
+package recaptcha
+
+import (
+	"encoding/json"
+	"log"
+	"net/url"
+
+	"github.com/sheva0914/MSc_2021-22_Mock_webserver/pkg/http_client"
+)
+
+const (
+	reCAPTCHAVerifyURL = "https://www.google.com/recaptcha/api/siteverify"
+)
+
+// Struct for reCAPTCHA score returned from Google
+type reCAPTCHAScoreStruct struct {
+	Success bool    `json:"success"`
+	Score   float64 `json:"score"`
+}
+
+func Verify(secretKey, urToken string) (isSuccess bool, score float64, err error) {
+	// Prepare request body with secret-key and UR token
+	reqBody := url.Values{
+		"secret":   {secretKey},
+		"response": {urToken},
+		// "remoteip": {"1.1.1.1"},
+	}
+
+	// Obtain reCAPTCHA score by sending request to Google
+	res, err := http_client.SendPostRequest(reCAPTCHAVerifyURL, reqBody)
+	if err != nil {
+		log.Println("Failed to get reCAPTCHA score: ", err)
+		return
+	}
+	log.Println(res)
+
+	// Verify score result by parsing response JSON
+	var scoreResult reCAPTCHAScoreStruct
+	if err = json.Unmarshal([]byte(res), &scoreResult); err != nil {
+		log.Println("Failed to unmarshal reCAPTCHA score: ", err)
+		return
+	}
+
+	isSuccess = scoreResult.Success
+	score = scoreResult.Score
+
+	return
+}
